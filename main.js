210926b957877d8e756970d04ca8f6a34e4db577
Oseremen Godstime <152567354+devandytech@users.noreply.github.com>
2025-02-28 01:48:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // buger menu nav
   const burgerMenu = document.querySelector(".burger-menu");
   const menu = document.querySelector(".menu");
 
@@ -6,4 +7,54 @@ document.addEventListener("DOMContentLoaded", function () {
     burgerMenu.classList.toggle("active");
     menu.classList.toggle("active");
   });
+  
+  
+  // card for offer animation and services 
+  const cards = document.querySelectorAll('.card, .service-offer');
+  window.addEventListener('scroll', () => {
+    cards.forEach((card) => {
+      const cardTop = card.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+      if (cardTop < windowHeight * 0.8) {
+        card.classList.add('appear');
+      }
+    });
+  });
+  
+  const zoomButton = document.querySelector('.explore-button');
+  if (zoomButton) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        zoomButton.classList.add('scrolled');
+      } else {
+      zoomButton.classList.remove('scrolled');
+      }
+    });
+  }
+  
+  // schedule animation 
+  const detailsElements = document.querySelectorAll('details');
+    detailsElements.forEach((detailsElement) => {
+      detailsElement.addEventListener('toggle', () => {
+        if (detailsElement.open) {
+          detailsElement.style.height = `${detailsElement.scrollHeight}px`;
+        } else {
+          detailsElement.style.height = '50px';
+        }
+      });
+  });
+  
+  // testimonial animation 
+  const testimonialCards = document.querySelectorAll('.review');
+  window.addEventListener('scroll', () => {
+    testimonialCards.forEach((card) => {
+      const cardTop = card.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+      if (cardTop < windowHeight * 0.8) {
+        card.classList.add('appear');
+      }
+    });
+  });
 });
+
+
